6649fad93c0ea6088c29e9f1430a3a4cf3e9b348
Maciej Topolewski <maciej@top-project.com.pl>
2020-12-15 13:57:26 +0100

    kombinuję z self.nowa_lista

--- magazyn_manager.py
+++ magazyn_manager.py
@@ -1,4 +1,4 @@
-import time
+import time, sys
 
 class Manager():
     """Warehouse management system"""
@@ -7,6 +7,7 @@ class Manager():
         self.saldo = 0
         self.lista = []
         self.magazyn = {}
+        self.nowa_lista = []
 
     def file_read(self, fhand):
         fhand = open(fhand)
@@ -74,7 +75,81 @@ class Manager():
 
     def file_write(self, fname, lista):
         fd = open(fname, "a")
-        for element in self.lista:
+        for element in self.nowa_lista:
             fd.write(str(element))
             fd.write("\n")
-        fd.write("stop")
+
+    def review(self):
+        saldo = 0
+        lista = []
+        magazyn = {}
+        index = -1
+        fhand = open(sys.argv[1])
+        while True:
+            y = int(sys.argv[2])
+            z = int(sys.argv[3])
+            fh = fhand.readline().strip()
+            if fh.startswith("saldo"):
+                money = fhand.readline().strip()
+                com = fhand.readline().strip()
+                saldo += int(money)
+                index += 1
+                if index >= y and index <= z:
+                    lista.append(fh)
+                    lista.append(money)
+                    lista.append(com)
+                fh
+            if fh.startswith("zakup"):
+                name = fhand.readline().strip()
+                price = int(fhand.readline().strip())
+                if price < 0:
+                    print("Błąd - cena nie może być mniejsza od zera")
+                    quit()
+                pieces = int(fhand.readline().strip())
+                if pieces < 0:
+                    print("Błąd - liczba sztuk nie może być mniejsza od zera")
+                    quit()
+                if (price * pieces) <= saldo:
+                    magazyn[name] = magazyn.get(name, 0) +pieces
+                    saldo -= (price * pieces)
+                    index += 1
+                    if index >= y and index <= z:
+                        lista.append(fh)
+                        lista.append(name)
+                        lista.append(price)
+                        lista.append(pieces)
+                    fh
+                else:
+                    print("Błąd - brak wystarczającej ilości środków na koncie")
+                    quit()
+            if fh.startswith("sprzedaż"):
+                name = fhand.readline().strip()
+                if name in magazyn:
+                    price = int(fhand.readline().strip())
+                    if price < 0:
+                        print("Błąd - cena nie może być mniejsza od zera")
+                        quit()
+                    pieces = int(fhand.readline().strip())
+                    if pieces < 0:
+                        print("Błąd - liczba sztuk nie może być mniejsza od zera")
+                        quit()
+                    if (price * pieces) <= saldo:
+                        magazyn[name] = magazyn.get(name, 0) -pieces
+                        saldo -= (price * pieces)
+                        index += 1
+                        if index >= y and index <= z:
+                            lista.append(fh)
+                            lista.append(name)
+                            lista.append(price)
+                            lista.append(pieces)
+                        fh
+                    else:
+                        print("Błąd - brak wystarczającej ilości środków na koncie")
+                        quit()
+                else:
+                    print("Brak takiego produktu w magazynie")
+                    quit()
+            if fh.startswith("stop"):
+                break
+        for element in lista:
+            print(element)
--- saldo.py
+++ saldo.py
@@ -2,13 +2,10 @@ import sys
 import time
 from magazyn_manager import *
 
-
+#po co zwracam listę? tam już sprawdziłem, a tutaj nadpisuje się dwukrotnie w pliku
 manager = Manager()
-(saldo, lista, magazyn) = manager.file_read(sys.argv[1])
-dodanie = int(sys.argv[2])
-komentarz = sys.argv[3]
-saldo += dodanie
-lista.append(dodanie)
-lista.append(komentarz)
-
-manager.file_write(sys.argv[1], lista)
+manager.file_read(sys.argv[1])
+manager.saldo += int(sys.argv[2])
+manager.nowa_lista.append(int(sys.argv[2]))
+manager.nowa_lista.append(sys.argv[3])
+manager.file_write(sys.argv[1], manager.nowa_lista)
