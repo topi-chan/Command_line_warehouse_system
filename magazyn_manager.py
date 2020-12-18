6ddb3839c069a4cc29764e095579b2f73e2ef82d
Maciej Topolewski <maciej@top-project.com.pl>
2020-12-18 20:07:13 +0100

    rozwiązany "przegląd" jako dekorator

--- magazyn_manager.py
+++ magazyn_manager.py
@@ -14,6 +14,7 @@ class Manager():
         self.manager_balance = None
         self.manager_review = None
         self.manager_warehouse = None
+        self.manager_overview = None
 
     def file_read(self, fhand):
         fhand = open(fhand)
@@ -42,78 +43,3 @@ class Manager():
         def decorator(cb):
             self.actions[name] = (cb, qty)
         return decorator
-
-    def review(self):
-        saldo = 0
-        lista = []
-        magazyn = {}
-        index = -1
-        fhand = open(sys.argv[1])
-        while True:
-            y = int(sys.argv[2])
-            z = int(sys.argv[3])
-            fh = fhand.readline().strip()
-            if fh.startswith("saldo"):
-                money = fhand.readline().strip()
-                com = fhand.readline().strip()
-                saldo += int(money)
-                index += 1
-                if index >= y and index <= z:
-                    lista.append(fh)
-                    lista.append(money)
-                    lista.append(com)
-                continue
-            if fh.startswith("zakup"):
-                name = fhand.readline().strip()
-                price = int(fhand.readline().strip())
-                if price < 0:
-                    print("Błąd - cena nie może być mniejsza od zera")
-                    quit()
-                pieces = int(fhand.readline().strip())
-                if pieces < 0:
-                    print("Błąd - liczba sztuk nie może być mniejsza od zera")
-                    quit()
-                if (price * pieces) <= saldo:
-                    magazyn[name] = magazyn.get(name, 0) +pieces
-                    saldo -= (price * pieces)
-                    index += 1
-                    if index >= y and index <= z:
-                        lista.append(fh)
-                        lista.append(name)
-                        lista.append(price)
-                        lista.append(pieces)
-                    continue
-                else:
-                    print("Błąd - brak wystarczającej ilości środków na koncie")
-                    quit()
-            if fh.startswith("sprzedaż"):
-                name = fhand.readline().strip()
-                if name in magazyn:
-                    price = int(fhand.readline().strip())
-                    if price < 0:
-                        print("Błąd - cena nie może być mniejsza od zera")
-                        quit()
-                    pieces = int(fhand.readline().strip())
-                    if pieces < 0:
-                        print("Błąd - liczba sztuk nie może być mniejsza od zera")
-                        quit()
-                    if (price * pieces) <= saldo:
-                        magazyn[name] = magazyn.get(name, 0) -pieces
-                        saldo -= (price * pieces)
-                        index += 1
-                        if index >= y and index <= z:
-                            lista.append(fh)
-                            lista.append(name)
-                            lista.append(price)
-                            lista.append(pieces)
-                        continue
-                    else:
-                        print("Błąd - brak wystarczającej ilości środków na koncie")
-                        quit()
-                else:
-                    print("Brak takiego produktu w magazynie")
-                    quit()
-            if fh.startswith(""):
-                break
-        for element in lista:
-            print(element)
--- mg.py
+++ mg.py
@@ -59,4 +59,79 @@ def manager_create():
     def manager_warehouse(manager, *any_argument):
         for arg in sys.argv[2:]:
             print(arg, manager.magazyn[arg])
+    @manager.assign("przegląd", 1)
+    def manager_overview(manager, *any_argument):
+        saldo = 0
+        lista = []
+        magazyn = {}
+        index = -1
+        fhand = open(sys.argv[1])
+        while True:
+            y = int(sys.argv[2])
+            z = int(sys.argv[3])
+            fh = fhand.readline().strip()
+            if fh.startswith("saldo"):
+                money = fhand.readline().strip()
+                com = fhand.readline().strip()
+                saldo += int(money)
+                index += 1
+                if index >= y and index <= z:
+                    lista.append(fh)
+                    lista.append(money)
+                    lista.append(com)
+                continue
+            if fh.startswith("zakup"):
+                name = fhand.readline().strip()
+                price = int(fhand.readline().strip())
+                if price < 0:
+                    print("Błąd - cena nie może być mniejsza od zera")
+                    quit()
+                pieces = int(fhand.readline().strip())
+                if pieces < 0:
+                    print("Błąd - liczba sztuk nie może być mniejsza od zera")
+                    quit()
+                if (price * pieces) <= saldo:
+                    magazyn[name] = magazyn.get(name, 0) +pieces
+                    saldo -= (price * pieces)
+                    index += 1
+                    if index >= y and index <= z:
+                        lista.append(fh)
+                        lista.append(name)
+                        lista.append(price)
+                        lista.append(pieces)
+                    continue
+                else:
+                    print("Błąd - brak wystarczającej ilości środków na koncie")
+                    quit()
+            if fh.startswith("sprzedaż"):
+                name = fhand.readline().strip()
+                if name in magazyn:
+                    price = int(fhand.readline().strip())
+                    if price < 0:
+                        print("Błąd - cena nie może być mniejsza od zera")
+                        quit()
+                    pieces = int(fhand.readline().strip())
+                    if pieces < 0:
+                        print("Błąd - liczba sztuk nie może być mniejsza od zera")
+                        quit()
+                    if (price * pieces) <= saldo:
+                        magazyn[name] = magazyn.get(name, 0) -pieces
+                        saldo -= (price * pieces)
+                        index += 1
+                        if index >= y and index <= z:
+                            lista.append(fh)
+                            lista.append(name)
+                            lista.append(price)
+                            lista.append(pieces)
+                        continue
+                    else:
+                        print("Błąd - brak wystarczającej ilości środków na koncie")
+                        quit()
+                else:
+                    print("Brak takiego produktu w magazynie")
+                    quit()
+            if fh.startswith(""):
+                break
+        for element in lista:
+            print(element)
     return manager
